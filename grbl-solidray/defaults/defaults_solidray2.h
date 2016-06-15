b4db90875344ecbb14c9557d06bdad8b4fcaa5f7
kitprinter3d <kitprinter3d@users.noreply.github.com>
2016-06-15 11:27:33 +0200

    Update defaults_solidray2.h

--- grbl-solidray/defaults/defaults_solidray2.h
+++ grbl-solidray/defaults/defaults_solidray2.h
@@ -36,7 +36,7 @@
   #define DEFAULT_Z_MAX_RATE 200.0  // mm/min
   #define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_Z_ACCELERATION (5.0*60*60) // 10*60*60 mm/min^2 = 5 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (3.0*60*60) // 10*60*60 mm/min^2 = 5 mm/sec^2
   #define DEFAULT_X_MAX_TRAVEL 200.0 // mm
   #define DEFAULT_Y_MAX_TRAVEL 200.0 // mm
   #define DEFAULT_Z_MAX_TRAVEL 160.0 // mm
