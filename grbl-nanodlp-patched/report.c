733e8b797e90692545beec63748b3cfaa9fe44ae
X3msnake <X3msnake@users.noreply.github.com>
2020-10-03 00:33:01 +0100

    Update report.c

--- grbl-nanodlp-patched/report.c
+++ grbl-nanodlp-patched/report.c
@@ -167,7 +167,7 @@ void report_grbl_help() {
                         "~ (cycle start)\r\n"
                         "! (feed hold)\r\n"
                         "? (current status)\r\n"
-                        "ctrl-x (reset Grbl)\r\n"));
+                        "^ (reset Grbl)\r\n"));
   #endif
 }
 
