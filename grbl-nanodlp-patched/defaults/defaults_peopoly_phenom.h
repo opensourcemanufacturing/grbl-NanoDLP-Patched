80a86fa073b5f85d99e894b55768b0846fdb3903
X3msnake <X3msnake@users.noreply.github.com>
2020-10-02 20:07:19 +0100

    fix homing direction mask

--- grbl-nanodlp-patched/defaults/defaults_peopoly_phenom.h
+++ grbl-nanodlp-patched/defaults/defaults_peopoly_phenom.h
@@ -54,7 +54,7 @@
   #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
   #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
   #define DEFAULT_HOMING_ENABLE 1  // false
-  #define DEFAULT_HOMING_DIR_MASK 1 // move positive dir
+  #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
   #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
   #define DEFAULT_HOMING_SEEK_RATE 150.0 // mm/min
   #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
